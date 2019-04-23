c4d8460d3e666bc9619ccc78704dcc5bf0d20866
pagidoproject <48087027+pagidoproject@users.noreply.github.com>
2019-04-23 03:11:47 -0700

    updated index.php
    by madhavi on 23rd April

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="description" content="Best Digital Marketing and Website Designing services in Thane,Mumbai|Pagido Consulting pvt ltd.
-  We are a website developing(bold) and a digital marketing(bold) company in Thane,India. Providing creative and customized online solutions including SEO(bold) ,PPC(bold) and Social Media">
+  <meta name="description" content="Best Digital Marketing and Website Designing services in Thane, Mumbai|Pagido Consulting pvt ltd.
+  We are a website developing and a digital marketing company in Thane, India. Providing creative and customized online solutions including SEO(bold) ,PPC(bold) and Social Media">
   <meta name="keywords" content="digital marketing agency, digital marketing agency in thane,digital marketing company in mumbai
   ,digital marketing agency in mumbai,advertising agencies in mumbai,Digital Marketing Agency,Social Media Agency,
-  Search Engine Optimization,SEO services,PPC marketing,Web development company,web design company in thane
+  Search Engine Optimization,SEO services,PPC marketing,Web development company,web design company in Thane
   ,corporate website design,Dynamic Website,E-commerce Website,Redesigning Website">
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
